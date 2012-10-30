7c3cac82288e87b160efa54bc2775c53674d49f6
Ryan <Ryan.phillips@ou.edu>
2012-10-29 23:42:25 -0500

    Added -r and -s along with detecting if it is an image or directory.

--- bwm.cpp
+++ bwm.cpp
@@ -14,30 +14,109 @@ int main(int argc, char* argv[])
 	int status;
 
 	//Create the temp directory that is going to be used for temp files
-	system("mkdir temp");
-	//loop(./temp,);
+	pid_t mkdirPID = fork();
+	if (mkdirPID == 0) {
+		execl ("/bin/mkdir", "mkdir", "temp", (char*)0);
+	} else {
+		wait (&status);
+	}
 
-	wgetPID = fork();
-	if (wgetPID == 0) {
+	//Loop through the arguments and see if -r or --recursive=NUM was specefied,
+	//otherwise
+	bool recursive = false;
+	string rnum, str2;
+	string URL = argv[argc-2];
+	
+	//If the URL is a directory, then see if -r or --recursive=NUM was specefied
+	if (URL.at(URL.length()-1) == '/') {
+		
+		for ( int i = 1; i < argc-1; i++) {
+			str2 = argv[i];
+			//check to see if -r is called
+			if (str2.at(1) == 'r' ) {
+				recursive=true;
+				rnum = argv[i+1];
+				break;
+			//check to see if --recursive is called, then set rnum to whatever is after the =
+			} else if (str2.at(2) == 'r') {
+				recursive =true;
+				rnum=argv[i];
+				rnum = rnum.substr(12, rnum.length()-1);
+				break;
+			}
+		}
+	} else if (recursive) {
+		//Need to exit because the user tried to call -r or --recursive=NUM on 
+		// a single image.
+		cout << "bwm will now exit because -r or --recursive=NUM was specefied without specifying a directory" << endl;
+		exit(0);
+	}
+	
+
+	//If -s or --save was not specefied, remove the directory
+	if (recursive) {
+		wgetPID = fork();
+		if (wgetPID == 0) {
 		//Need to change this depending on what time of image or folder, need if statement
 		//to see if we have a directory and if its just an image we just need to use the same
 		//switch statement from project 2.
 
 		//use -r and -l # to recursivly download to a certain depth.
-		execl ("/usr/bin/wget", "wget","-r","-l","2", "-P","./temp/","-A", "jpg,tif,png", argv[argc-2], (char*)0); 
-	}
-	else {
-		wait(&status);
+			execl ("/usr/bin/wget", "wget", "-r","-l",rnum.c_str(), "-P","./temp/","-A", "jpg,tif,png", argv[argc-2], (char*)0); 
+		}
+		else {
+			wait(&status);
+		}
+	} else {
+		//Need to do something if the recursive call is not called, need to also exit if the -r is
+		//called for a single image. 
+		switch (URL.at(URL.length()-2) ) {
+		case 'p': 
+			cout << "***Image is a jpg" << endl;
+	    		execl ("/usr/bin/wget", "wget", "-q", "-O", "./image.jpg", argv[argc-2], (char*)0); 
+			break;
+		case 'n': 
+			cout << "***Image is a png" << endl;
+			execl ("/usr/bin/wget", "wget", "-q", "-O", "./image.png", argv[argc-2], (char*)0); 
+			break;
+		case 'i': 
+			cout << "***Image is a TIFF" << endl;
+			execl ("/usr/bin/wget", "wget", "-O", "./image.tif", argv[argc-2], (char*)0); 
+			break;
+ 		}
 	}
 	
+	//loop(./temp,);
 	string name = "./temp";
 	string watermark = "/watermark";
-	loop(name,3,0,watermark);
+	//loop(name,3,0,watermark);
 	//Call wm for a single image
 	wm (argv[argc-2], argv[argc-1]);
-	//system ("rmdir temp");
-}
 
+	//Loop through the arguments and see if -s or --save was specefied
+	//if so, make sure we save the original file.
+	bool save = false;
+	string str;
+	for ( int i = 1; i < argc-1; i++) {
+		str = argv[i];
+		if (str.at(1) == 's' || str.at(2) == 's') {
+			save=true;
+		}	
+	}
+
+	//If -s or --save was not specefied, remove the directory
+	if (!save) {
+		pid_t rmdirPID = fork();
+		if (rmdirPID == 0) {	
+			cout << "remove the directory" << endl;
+			execl("/bin/rm","rm","-r","temp",(char*)0);
+		}
+		else {
+			wait(&status);
+		}
+	}
+}
+/**
 int loop(string name, int max, int current, string watermark){
 
 	DIR* dir;
@@ -59,7 +138,7 @@ int loop(string name, int max, int current, string watermark){
 	return 0;
 
 }
-
+**/
 int wm( string file,  string watermark){
 	
 string filePath = file;
@@ -169,10 +248,7 @@ cout << filePath << " " << wmPath << endl;
 		     }
 		}	
    	  }	
-    }
-           exit(0);  /* parent exits with code 0 since everything completed successfully */       
+    }     
         
-   
-
     return 0;
 }
